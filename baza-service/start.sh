e196be23e446f8d21d3964b3d76306625dd05a6d
prasanta <prasantakakati1994@gmail.com>
2019-06-18 02:27:09 +0530

    modified start.sh of baza-service

--- baza-service/start.sh
+++ baza-service/start.sh
@@ -3,6 +3,6 @@ then
     ./baza-service -w $POOLSERVER_WALLET_FILE -p $POOLSERVER_WALLET_PASSWORD --rpc-password $POOLSERVER_RPC_PASSWORD\
         --bind-address 0.0.0.0 --daemon-address $DAEMON_HOST --daemon_port $DAEMON_PORT
 else
-    echo "file not found"
+    echo "file not found" $POOLSERVER_WALLET_FILE
     exit 0
 fi
