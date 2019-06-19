e1e7daa3ef51173d16941575a229e15e4d891c48
prasanta <prasantakakati1994@gmail.com>
2019-06-19 07:28:59 +0530

    fixed typo

--- baza-service/start.sh
+++ baza-service/start.sh
@@ -1,12 +1,7 @@
-if [ -f $POOLSERVER_WALLET_FILE ]
+if [ -f $WALLETSERVER_WALLET_FILE ]
 then
-    echo $POOLSERVER_RPC_PASSWORD
-    echo $POOLSERVER_WALLET_FILE
-    echo $POOLSERVER_WALLET_PASSWORD
-    echo $DAEMON_HOST
-    echo $DAEMON_PORT
-    /bin/baza-service -w $POOLSERVER_WALLET_FILE -p $POOLSERVER_WALLET_PASSWORD --rpc-password $POOLSERVER_RPC_PASSWORD --bind-address 0.0.0.0 --daemon-address $DAEMON_HOST --daemon_port $DAEMON_PORT
+    /bin/baza-service -w $WALLETSERVER_WALLET_FILE -p $WALLETSERVER_WALLET_PASSWORD --rpc-password $WALLETSERVER_RPC_PASSWORD --bind-address 0.0.0.0 --daemon-address $DAEMON_HOST --daemon_port $DAEMON_PORT
 else
-    echo "file not found" $POOLSERVER_WALLET_FILE
+    echo "file not found" $WALLETSERVER_WALLET_FILE
     exit 0
 fi
