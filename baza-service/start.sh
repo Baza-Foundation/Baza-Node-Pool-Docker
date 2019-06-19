a331ee9c3e9ba14c56419afb52a36bb280de8951
prasanta <prasantakakati1994@gmail.com>
2019-06-19 07:25:43 +0530

    test

--- baza-service/start.sh
+++ baza-service/start.sh
@@ -1,7 +1,11 @@
 if [ -f $POOLSERVER_WALLET_FILE ]
 then
-    /bin/baza-service -w $POOLSERVER_WALLET_FILE -p $POOLSERVER_WALLET_PASSWORD --rpc-password $POOLSERVER_RPC_PASSWORD\
-        --bind-address 0.0.0.0 --daemon-address $DAEMON_HOST --daemon_port $DAEMON_PORT
+    echo $POOLSERVER_RPC_PASSWORD
+    echo $POOLSERVER_WALLET_FILE
+    echo $POOLSERVER_WALLET_PASSWORD
+    echo $DAEMON_HOST
+    echo $DAEMON_PORT
+    /bin/baza-service -w $POOLSERVER_WALLET_FILE -p $POOLSERVER_WALLET_PASSWORD --rpc-password $POOLSERVER_RPC_PASSWORD --bind-address 0.0.0.0 --daemon-address $DAEMON_HOST --daemon_port $DAEMON_PORT
 else
     echo "file not found" $POOLSERVER_WALLET_FILE
     exit 0
