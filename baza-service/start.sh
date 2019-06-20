b01848d7f7d97fb5dd856e230e56f1cad13f72a3
prasanta <prasantakakati1994@gmail.com>
2019-06-21 02:11:04 +0530

    added logs saving to daemon and service and persistency to redis

--- baza-service/start.sh
+++ baza-service/start.sh
@@ -1,6 +1,6 @@
 if [ -f $WALLETSERVER_WALLET_FILE ]
 then
-    /bin/baza-service -w $WALLETSERVER_WALLET_FILE -p $WALLETSERVER_WALLET_PASSWORD --rpc-password $WALLETSERVER_RPC_PASSWORD --bind-address 0.0.0.0 --daemon-address $DAEMON_HOST --daemon-port $DAEMON_PORT
+    /bin/baza-service -w $WALLETSERVER_WALLET_FILE -p $WALLETSERVER_WALLET_PASSWORD --rpc-password $WALLETSERVER_RPC_PASSWORD --bind-address 0.0.0.0 --daemon-address $DAEMON_HOST --daemon-port $DAEMON_PORT --log-file /var/log/bazaservice/service.log
 else
     echo "file not found" $WALLETSERVER_WALLET_FILE
     exit 0
